eb62c263f08312dfbe09e4c63a3106ec6fce2ed2
pignoi <pigno1@hotmail.com>
2024-05-16 16:12:01 +0800

    fix start_collect.sh with more convenient method.

--- start_collect.sh
+++ start_collect.sh
@@ -1,3 +1,4 @@
+## 此脚本可以用于快速开启收集程序，如果需要用到bilibili的收集程序，需要先在指定目录下修改对应的userinfo_sample.json。该脚本提供一个模版，如果需要循环等功能请自行添加。
 #!/bin/bash
 
 WORK_DIR=/home/webapp/danmaku-stats    # change your workdir here
@@ -9,9 +10,9 @@ if  [ ! -d $WORK_DIR/dbs ]; then
     mkdir $WORK_DIR/dbs
 fi
 
-if  [ ! -d $WORK_DIR/userinfo.json ] && [ $PLATFORM == bilibili ]; then
+if  [ ! -f $WORK_DIR/userinfo.json ] && [ $PLATFORM == bilibili ]; then
     echo "There is not your bilibili user information. Please check userinfo_sample.json and https://nemo2011.github.io/bilibili-api/#/get-credential"
     exit 1
 fi
 
-docker run -d -v $WORK_DIR/dbs/:/app/dbs -v $WORK_DIR/userinfo.json:/app/dbs -e PLATFORM=$PLATFORM -e ROOM_ID=$ROOM_ID --restart always --name $DOCKER_NAME danmaku-collector
+docker run -d -v $WORK_DIR/dbs/:/app/dbs -v $WORK_DIR/userinfo.json:/app/userinfo.json -e PLATFORM=$PLATFORM -e ROOM_ID=$ROOM_ID --restart always --name $DOCKER_NAME pignoi/danmaku-collector:v1.3
